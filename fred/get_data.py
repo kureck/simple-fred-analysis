e5dc20b5629d81e703d661ba90880ae009d41838
Eric Kureck <ekureck@bionexo.com>
2018-05-04 18:59:28 -0300

    creates methods to organize

--- fred/get_data.py
+++ fred/get_data.py
@@ -1,15 +1,35 @@
 import os
 import json
+import argparse
 import urllib.request
 import pandas as pd
 from sqlalchemy import create_engine
+
+# parser = argparse.ArgumentParser()
+# incremental_group = parser.add_mutually_exclusive_group(required=False)
+# parser.add_argument('--origin', '-o', help='Origin datasource', required=True)
+# parser.add_argument('--target', '-t', help='Target datasource', required=True)
+# parser.add_argument('--country', '-c', help='Application country', required=True)
+# parser.add_argument('--table', '-T', help='Table name', required=False)
+# incremental_group.add_argument('--incremental',
+#                                dest='incremental',
+#                                action='store_true',
+#                                help='Flag to set if job is full or incremental')
+# parser.set_defaults(incremental=False)
+#
+# args = parser.parse_args()
 #logging
-# add last_modified_date
+# add argparse
 
-# env varibles
-engine = create_engine('postgresql://postgres:postgres@postgres:5432/fred')
+def db_engine():
+    DB_HOST = os.getenv('DB_HOST', None)
+    DB_PORT = os.getenv('DB_PORT', None)
+    DB_USER = os.getenv('DB_USER', None)
+    DB_PASSWD = os.getenv('DB_PASSWD', None)
+    DB_NAME = os.getenv('DB_NAME', None)
+    connection_url = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWD, DB_HOST, DB_PORT, DB_NAME)
+    return create_engine(connection_url)
 
-# can be transformed into a function
 def transform(json_data):
     df = pd.DataFrame(json_data)
     df = df.drop(['realtime_end', 'realtime_start'], axis=1)
@@ -17,26 +37,26 @@ def transform(json_data):
     return df
 
 def update(df, table_name, incremental=False):
+    engine = db_engine()
     if incremental:
         df.to_sql(table_name, engine, schema='fred', if_exists='append', index=False) ## To be used as incremental
     else:
         df.to_sql(table_name, engine, schema='fred', if_exists='replace', index=False) ## To be used with initial flag
 
-if __name__ == '__main__':
+def url(series_id):
     FRED_API_KEY = os.getenv('FRED_API_KEY', None)
+    serie = series_id.upper()
+    return "https://api.stlouisfed.org/fred/series/observations?series_id={}&api_key={}&file_type=json".format(serie, FRED_API_KEY)
 
-    GDPC1_URL = "https://api.stlouisfed.org/fred/series/observations?series_id=GDPC1&api_key={}&file_type=json".format(FRED_API_KEY)
-    UMCSENT_URL = "https://api.stlouisfed.org/fred/series/observations?series_id=UMCSENT&api_key={}&file_type=json".format(FRED_API_KEY)
-    UNRATE_URL = "https://api.stlouisfed.org/fred/series/observations?series_id=UNRATE&api_key={}&file_type=json".format(FRED_API_KEY)
+def fetch_data(series_id):
+    serie_url = url(series_id)
+    serie_json = urllib.request.urlopen(serie_url).read().decode('utf8')
+    return json.loads(serie_json)
 
-    gdpc1_json = urllib.request.urlopen(GDPC1_URL).read().decode('utf8')
-    gdpc1 = json.loads(gdpc1_json)
-
-    umcsent_json = urllib.request.urlopen(UMCSENT_URL).read().decode('utf8')
-    umcsent = json.loads(gdpc1_json)
-
-    unrate_json = urllib.request.urlopen(UNRATE_URL).read().decode('utf8')
-    unrate = json.loads(gdpc1_json)
+if __name__ == '__main__':
+    gdpc1 = fetch_data('gdpc1')
+    umcsent = fetch_data('umcsent')
+    unrate = fetch_data('unrate')
 
     gdpc1_df = transform(gdpc1['observations'])
     umcsent_df = transform(umcsent['observations'])
